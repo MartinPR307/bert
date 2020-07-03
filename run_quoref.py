46d8fc86792e2fa6086ce363b9eca97d6d59f0e0
xiaoyli@outlook.com <xiaoyli@outlook.com>
2020-07-03 13:31:57 +0800

    fix

--- run_quoref.py
+++ run_quoref.py
@@ -265,7 +265,7 @@ def read_squad_examples(input_file, is_training):
           #   is_impossible = qa["is_impossible"]
           if (len(qa["answers"]) != 1) and (not is_impossible):
             # raise ValueError("For training, each question should have exactly 1 answer.")
-            raise ValueError(qa,） #  qa["answers"])
+            raise ValueError(qa) #  qa["answers"])
 
           is_impossible = False 
           if not is_impossible:
